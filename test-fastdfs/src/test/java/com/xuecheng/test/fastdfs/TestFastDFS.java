a79d80ba9ff4861b50f4f98137ce9c557e567807
git-test <50097603+XiChuGit@users.noreply.github.com>
2019-07-30 16:00:18 +0800

    Update TestFastDFS.java

--- test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 public class TestFastDFS {
     public static void main(String[] args) {
         System.out.println("第一次修改");
+         System.out.println("第二次修改");
     }
     //上传文件
     @Test
